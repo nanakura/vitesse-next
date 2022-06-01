08674c5e00237c85882be42588d493e84a54a9bf
nanakura <1749094641@qq.com>
2022-06-01 23:19:42 +0800

    chore: basic components

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@ const UnoCSS = require('@unocss/webpack').default
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
-    // config.module.rules.push({
-    //   test: /\.(md|mdx)$/,
-    //   use: [
-    //     options.defaultLoaders.babel,
-    //     {
-    //       loader: '@mdx-js/loader',
-    //       options: pluginOptions.options,
-    //     },
-    //   ],
-    // })
     config.plugins = [
       ...config.plugins,
       // require('unplugin-auto-import/webpack')({
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
+import 'uno.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import DefaultLayout from '../layouts/DefaultLayout'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
